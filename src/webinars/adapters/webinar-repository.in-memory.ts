4c57c70cb47e8e6a0213ef1491b86ca59545621b
luffy <78445240+yanihadid@users.noreply.github.com>
2025-01-20 23:12:18 +0100

    book webinars + test

--- src/webinars/adapters/webinar-repository.in-memory.ts
+++ src/webinars/adapters/webinar-repository.in-memory.ts
@@ -3,7 +3,16 @@ import { IWebinarRepository } from 'src/webinars/ports/webinar-repository.interf
 
 export class InMemoryWebinarRepository implements IWebinarRepository {
   constructor(public database: Webinar[] = []) {}
+  
+  //Permet d'ajouter un webinar
   async create(webinar: Webinar): Promise<void> {
     this.database.push(webinar);
   }
+
+  //Permet de récupérer le webinar selon son id
+  async findById(webinarId: String): Promise<Webinar | null> {
+    const webinar = this.database.find(w => w.props.id === webinarId);
+    return webinar || null;
+  }
+
 }
--- src/webinars/use-cases/book-seat.ts
+++ src/webinars/use-cases/book-seat.ts
@@ -1,9 +1,11 @@
-import { IMailer } from 'src/core/ports/mailer.interface';
+import { request } from 'http';
+import { Email, IMailer } from 'src/core/ports/mailer.interface';
 import { Executable } from 'src/shared/executable';
 import { User } from 'src/users/entities/user.entity';
 import { IUserRepository } from 'src/users/ports/user-repository.interface';
 import { IParticipationRepository } from 'src/webinars/ports/participation-repository.interface';
 import { IWebinarRepository } from 'src/webinars/ports/webinar-repository.interface';
+import { Participation } from '../entities/participation.entity';
 
 type Request = {
   webinarId: string;
@@ -18,7 +20,55 @@ export class BookSeat implements Executable<Request, Response> {
     private readonly webinarRepository: IWebinarRepository,
     private readonly mailer: IMailer,
   ) {}
+
   async execute({ webinarId, user }: Request): Promise<Response> {
+
+    const webinar = await this.webinarRepository.findById(webinarId);
+
+    if (!webinar) {
+      throw new Error('Webinar introuvable');
+    }
+
+    const webinarParticipations = await this.participationRepository.findByWebinarId(webinarId);
+
+    const alreadyRegistered = webinarParticipations.some(
+      (participation) => participation.props.userId === user.props.id,
+    );
+
+    if (alreadyRegistered) {
+      throw new Error('L’utilisateur est déjà inscrit à ce webinar');
+    }
+
+    if (webinar.hasNotEnoughSeats()) {
+      throw new Error('Le webinar est complet');
+    }
+
+    const participation = new Participation({
+      userId: user.props.id,
+      webinarId,
+    });
+
+    await this.participationRepository.save(participation);
+
+   
+    webinar.addUser();
+    await this.webinarRepository.create(webinar);
+
+ 
+    const organizer = await this.userRepository.findById(webinar.props.organizerId);
+
+    if (!organizer) {
+      throw new Error('Organisateur introuvable');
+    }
+
+    const email: Email = {
+      to: organizer.props.email,
+      subject: 'Nouvelle inscription au webinar',
+      body: `Bonjour,\nUn nouvel utilisateur s'est inscrit à votre webinar.`,
+    };
+
+    await this.mailer.send(email);
+
     return;
   }
 }
